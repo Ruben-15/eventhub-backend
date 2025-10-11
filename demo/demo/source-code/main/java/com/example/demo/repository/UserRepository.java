aef67bc69def9e86bbb5abdbf664ec177d582022
Ruben <rubenkoshy30@gmail.com>
2025-10-11 14:12:37 +0530

    Update UserRepository.java

--- demo/demo/source-code/main/java/com/example/demo/repository/UserRepository.java
+++ demo/demo/source-code/main/java/com/example/demo/repository/UserRepository.java
@@ -1,1 +1,13 @@
+package com.example.demo.repository;
 
+import com.example.demo.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+    // Allows searching for the user by their username (used for login and registration)
+    Optional<User> findByUsername(String username);
+}
